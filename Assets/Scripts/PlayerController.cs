de81d7569eb8f6c30bfb66ef657b9c5d1d23671b
Marc-Haze <marcosdrdc7@gmail.com>
2022-01-19 19:02:50 +0000

    Adding Features to MiniGame

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+
     public float speed = 0;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     private float movementZ;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject endMessage;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>(); 
+        count = 0;  
+        SetCountText(); 
+        endMessage.SetActive(false);
     }
 
     void OnMove(InputValue movementValue){
@@ -23,9 +31,24 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText(){
+        countText.text = "Puntos: " + count.ToString();
+        if(count >= 5){
+            endMessage.SetActive(true);
+        }
+    }
+
     void FixedUpdate(){
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
     
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            count +=1;
+            SetCountText();
+        }
+    }
 }
+
